41a2f6fe69dba38031064bc4bfaf20bb562be057
VishalDevelooperengineer <vk4993102@gmail.com>
2022-09-24 01:58:35 +0530

    tutorial : 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,25 @@
 import "package:flutter/material.dart";
 
-void main(){
-  runApp(new MaterialApp(home:new Text("hello Duniya")));
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Tutorial 2 first app",
+      home: Scaffold(
+        // ignore: prefer_const_constructors
+        appBar: AppBar(title: Text("Hello world App Bar")),
+        body: const Center(
+          heightFactor: 20,
+          widthFactor: 20,
+          child:Text("Hello world first App"),
+        ),
+      ),
+    );
+  }
 }
